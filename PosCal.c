c6dcff7dc42f1382b3b6e84aa0fb9ad9483a4209
오우빈 <@DESKTOP-OJTHB5T>
2023-04-04 11:15:56 +0900

    초기 커밋.

---
+++ PosCal.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#define MAX_SIZE 100
+
+typedef struct {
+	int top;
+	int data[MAX_SIZE];
+} Stack;
+
+void init(Stack* s) {
+	s->top = -1;
+}
+
+int is_empty(Stack* s) {
+	return s->top == -1;
+}
+
+int is_full(Stack* s) {
+	return s->top == MAX_SIZE - 1;
+}
+
+void push(Stack* s, int item) {
+	if (is_full(s)) {
+		fprintf(stderr, "Stack Full!\n");
+		return;
+	}
+	s->data[++s->top] = item;
+}
+
+int pop(Stack* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "Invalid character!\n");
+		exit(1);
+	}
+	return s->data[s->top--];
+}
+
+int eval(const char* exp) {
+	int op1, op2, value, i = 0;
+	int len = strlen(exp);
+	char ch;
+	Stack s;
+	init(&s);
+	for (i = 0; i < len; i++) {
+		ch = exp[i];
+		if (ch >= '0' && ch <= '9') {
+			push(&s, ch - '0');
+		}
+		else {
+			op2 = pop(&s);
+			op1 = pop(&s);
+			switch (ch) {
+			case '+': push(&s, op1 + op2); break;
+			case '-': push(&s, op1 - op2); break;
+			case '*': push(&s, op1 * op2); break;
+			case '/': push(&s, op1 / op2); break;
+			default:
+				fprintf(stderr, "Invalid character\n");
+				exit(1);
+			}
+		}
+	}
+	return pop(&s);
+}
+
+int main(void) {
+	char exp[MAX_SIZE];
+	scanf_s("%s", exp, MAX_SIZE);
+	int result = eval(exp);
+	printf("Result: %d\n", result);
+	return 0;
+}
